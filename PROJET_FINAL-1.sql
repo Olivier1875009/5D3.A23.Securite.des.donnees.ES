03538b7863ade33fd4036caacf7239dd1321c670
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-10-27 12:23:59 -0400

    update the script

---
+++ PROJET_FINAL-1.sql
@@ -0,0 +1,6 @@
+-- Script #1
+-- Drop the pluggable database
+DROP PLUGGABLE DATABASE PROJET4205D3 INCLUDING DATAFILES;
+
+-- Create the pluggable database
+CREATE PLUGGABLE DATABASE PROJET4205D3 ADMIN USER homer_simpson IDENTIFIED BY nsBWo5k38d8m create_file_dest='/home/oracle';
---
+++ PROJET_FINAL-2.sql
@@ -0,0 +1,6 @@
+-- Script #2
+-- Modify the pdbs
+ALTER DATABASE OPEN;
+
+-- Give privileges to the user
+GRANT ALL PRIVILEGES TO homer_simpson;
